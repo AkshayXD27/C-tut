1a82ba505bf84b9699d905086d447015cd2cee75
AkshayXD27 <lostamongstars27@gmail.com>
2026-04-05 13:08:29 +0530

    upd

--- formatspec.c
+++ formatspec.c
@@ -12,5 +12,13 @@ int main(){
     printf("%.2lf\n",pi); // for double lf = long floating number 
      // .n means the length of numbers tht should be considered or printed
      printf("%c\n",currency); //char
-     printf("%s",tut); //string
+     printf("%s\n",tut); //string
+     int a = 1 ,b = 10 ,c = 100;
+     printf("%4d\n",a); // 4d means the number of char / space assigned to the left side 
+     printf("%-4d\n",b); // space assigned to right side
+     printf("%04d\n",c); //instead of space it will be filled by 0
+        c = - 100;     
+     printf("%+d\n",a); 
+     printf("%+d\n",b); 
+     printf("%+d\n",c);  // prints + if num is pos - if neg
 }
