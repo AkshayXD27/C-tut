53f5b36349729a627e0c2c9f83c6a5f917dfffe6
AkshayXD27 <lostamongstars27@gmail.com>
2026-04-06 15:49:06 +0530

    on meth

---
+++ meth.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main(){
+    int x = 10.2;
+    x = sqrt(x);
+    x = pow(x,2);
+    x = log(x);
+    x = sin(x);
+    x = cos(x);
+    x = tan(x);  
+    x = abs(x);
+    x = round(x);
+
+    printf("%d",x);
+
+}
