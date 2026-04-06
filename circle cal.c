dce041b149ac43f7171d60101fa21a6ea1ad9a60
AkshayXD27 <lostamongstars27@gmail.com>
2026-04-06 15:55:25 +0530

    cont

---
+++ circle cal.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float radius,area = 0.0f;
+    double pi = 3.14159265359;
+
+    printf("Enter radius of circle: ");
+    scanf("%d",&radius);
+    
+}
