1b42abed3ceb1179393880159f6907d9e1010ec6
AkshayXD27 <lostamongstars27@gmail.com>
2026-04-06 16:53:36 +0530

    compound cal

--- compound cal.c
+++ compound cal.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
 #include <math.h>
+
+int main() {
+    double principal = 0.0;
+    double rate = 0.0;
+    int years =0;
+    int time = 0;
+    double total = 0.0;
+
+
+    printf("Compound Interest Calculator : \n");
+    printf("Enter principal : ");
+    scanf("%lf",&principal);
+    printf("Enter rate : ");
+    scanf("%lf",&rate);
+    printf("Enter time : ");
+    scanf("%d",&time);
+    printf("Enter years : ");
+    scanf("%d",&years);
+     rate = rate / 100;
+     total = principal * pow(1+rate/time,time*years);
+    
+     printf("total amount = %.2lf",total);
+
+}
