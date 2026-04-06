f02d9d0492b485883c5bb39ed39ee6f65a7a2adc
AkshayXD27 <lostamongstars27@gmail.com>
2026-04-06 15:22:38 +0530

    reset

---
+++ formatspec.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    int age = 25;
+    float price = 99.99;
+    double pi = 3.141592;
+    char currency = 'c';
+    char tut[] = "learning from bro code";
+
+    printf("%d\n",age);
+    printf("%.1f\n",price); // for float
+    printf("%.2lf\n",pi); // for double lf = long floating number 
+     // .n means the length of numbers tht should be considered or printed
+     printf("%c\n",currency); //char
+     printf("%s\n",tut); //string
+     int a = 1 ,b = 10 ,c = 100;
+     printf("%4d\n",a); // 4d means the number of char / space assigned to the left side 
+     printf("%-4d\n",b); // space assigned to right side
+     printf("%04d\n",c); //instead of space it will be filled by 0
+        c = - 100;     
+     printf("%+d\n",a); 
+     printf("%+d\n",b); 
+     printf("%+d\n",c);  // prints + if num is pos - if neg
+
+        /* percision 
+            the default behavior of c is to print 6 numbers after decimal point
+
+        */
+
+
+}
---
+++ madlibsgame.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char adj [30] = "";
+    char noun [30] = "";
+    char adv [30] = "";
+    char verb [30] = "";
+    char ex [30] = "";
+    char place[30] = "";
+    printf("Enter the noun : ");
+    fgets(noun,sizeof(noun),stdin);
+    noun[strlen(noun) -1] = '\0';
+    printf("Enter the adj : ");
+    fgets(adj,sizeof(adj),stdin);
+    adj[strlen(adj) -1] = '\0';
+    printf("Enter the adv : ");
+    fgets(adv,sizeof(adv),stdin);
+    adv[strlen(adv) -1] = '\0';
+    printf("Enter the verb : ");
+    fgets(verb,sizeof(verb),stdin);
+    verb[strlen(verb)-1] ='\0';
+    printf("Enter the ex : ");
+    fgets(ex,sizeof(ex),stdin);
+    ex[strlen(ex) -1] = '\0';
+    printf("Enter the place : ");
+    fgets(place,sizeof(place),stdin);
+    place[strlen(place) -1] = '\0';
+
+    printf("%s! I yelled as I stepped into the%s I couldn't believe my eyes—there was a %s %s %sing. right in the middle of the room! A group of %s watched %s from the corner. It was the strangest thing I'd ever seen.",ex,place,adj,noun,verb,noun,adv);
+
+
+}
