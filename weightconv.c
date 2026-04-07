8723c5ef90b818b5f4ad407d811fa5f280a73b06
AkshayXD27 <lostamongstars27@gmail.com>
2026-04-07 12:02:53 +0530

    Weight conversion problem/project

--- weightconv.c
+++ weightconv.c
@@ -2,5 +2,34 @@
 #include <string.h>
 
 int main(){
-    
+    float p = 0.0f; // 0.0f indicates tht the variable is floating type for other user purposes
+    float k = 0.0f;
+    int c = 0;
+    double conv = 0.0;
+    double ui = 0.0; //ui = user input
+    p = 0.45359;
+    k = 2.20462;
+    // use double for more precision
+    printf("Weight Conversion Calculator\n");
+    printf("1. Kilograms to pounds\n");
+    printf("2. Pounds to Kilograms\n");
+    printf("Enter your choice (1/2): ");
+    scanf("%d",&c);
+
+    if (c == 1){
+        printf("Enter the kilogram: ");
+        scanf("%lf",&ui); // lf = long floating number
+        conv =  ui * k; // kg into pounds 
+        printf("After conversion: %lf Pounds",conv);
+    }
+   else if (c == 0){
+        printf("Enter the Pounds: ");
+        scanf("%lf",&ui); // lf = long floating number
+        conv =  ui * p; // pounds into kg
+        printf("After conversion: %.4lf Kilogram",conv);
+    }
+    else {
+        printf("Conversion doesnt exist");
+    }
+
 }
