38fb3ab6abe37fa755d541ae1d6cdf8dacdfb3ac
AkshayXD27 <lostamongstars27@gmail.com>
2026-04-07 11:42:48 +0530

    checking if string is empty

--- if.c
+++ if.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 int main(){
     bool isStudent = true;
     if (isStudent) {
@@ -7,6 +8,18 @@ int main(){
 
     }
     else {
-        printf("You are not a student ");
+        printf("You are not a student \n");
     }
+
+    char name[30] = "";
+    printf("Enter your name :");
+    fgets(name,sizeof(name),stdin);
+    name[strlen(name) - 1] = '\0';
+    if (strlen(name) == 0){
+        printf("You didnt enter a name: ");
+    }
+    else {
+        printf("You entered a name and name = %s",name);
+    }
+
 }
