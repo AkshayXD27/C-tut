db8348b2c772a9e6475f262ed3398e8b11c9109c
AkshayXD27 <lostamongstars27@gmail.com>
2026-04-19 18:06:12 +0530

    nested if

--- nestedif.c
+++ nestedif.c
@@ -3,5 +3,29 @@
 
 int main(){
     float price = 10.0;
-    printf("%d",price);
+    bool isStudent = false;
+    bool isSenior = true;
+
+    if (isStudent){
+        if (isSenior)
+        {
+            printf("You get a student discount of 10%");
+            printf("You get a senior discount of 20%");
+            price *= 0.7; // if we do .30 we get discount amount which we have to sub from org price instead of tht we are mult by .7 to get after disc price
+        }
+        else {
+            printf("You get a student discount of 10%");
+            price *= 0.9;
+        }
+    }
+    else {
+        if (isSenior)
+        {
+            printf("You get a senior discount of 20%");
+            price *= 0.8;
+        }
+        
+    }
+
+    printf("The price of the movie ticket is: $%.2f",price);
 }
