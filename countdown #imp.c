248b119321af2b460447b5e85791f025fa1a1b28
AkshayXD27 <lostamongstars27@gmail.com>
2026-04-19 23:09:24 +0530

    countdown

--- countdown #imp.c
+++ countdown #imp.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+// #include <windows.h> for windows
+#include <unistd.h> // for linux or mac
+
+int main(){
+    for (int i = 10; i>=0; i--){
+        // Sleep(1000);  for windows time in milliseconds
+        sleep(1); // for linux or mac time given in seconds and uses small s
+        printf("%d\n",i);
+    }
+    printf("Happy new year!");
+}
