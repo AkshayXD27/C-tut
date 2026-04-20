00d84c841c5e93cc9a1d3c322ca41bff5d00014b
AkshayXD27 <lostamongstars27@gmail.com>
2026-04-20 11:03:30 +0530

    arrays

--- array.c
+++ array.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    int a [] = {10,20,39,45,56,24,213,31,100};
+    int a [] = {0,13};
     int size = sizeof(a) / sizeof(a[0]); // size of the whole variable / size of one single variable 
     for (int i=0;i<size;i++){
         printf("%d ",a[i]);
