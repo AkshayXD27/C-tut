780f1081e6848810cd7188924d3e38c36dd6f8d1
AkshayXD27 <lostamongstars27@gmail.com>
2026-04-20 12:14:51 +0530

    struct of arrays

---
+++ arrayofstru.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct {
+    char cars[30];
+    int year;
+    int price;
+} cars;
+
+int main(){
+    cars c1[] = {{"BMW",2025,400000},{"MERCEDES",2027,9000000}};
+    int rows = sizeof(c1) / sizeof(c1[0]);
+    for(int i = 0; i<rows;i++){
+        printf("%s %d %d\n",c1[i].cars,c1[i].year,c1[i].price);
+    }
+}
